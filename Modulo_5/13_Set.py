b263ec80a718a14c904343a23b46ba49791fa175
Yehudi <yehudi21903@estudante.se.df.gov.br>
2023-11-29 12:45:27 -0300

    Sets em python

---
+++ Modulo_5/13_Set.py
@@ -0,0 +1,25 @@
+# Set- Nao mostra valores repitidos
+numeros = [2, 2, 5, 8]
+frutas = {'Maça', 'Mamao', 'Banana', 'Maça', 'Uva'}
+
+# Convertendo para Set
+set_numeros = set(numeros)
+set_frutas = set(frutas)
+
+print(set_numeros)
+print(set_frutas)
+# Adicionando novos valores
+set_numeros.add(10)
+print(set_numeros)
+
+# Conjuntos
+
+numeros1 = [2, 2, 5, 8]
+numeros2 = [2, 2, 3, 9]
+
+a = set(numeros1)
+b = set(numeros2)
+
+print(a.symmetric_difference(b))
+print(a.intersection(b))
+print(a.union(b))
